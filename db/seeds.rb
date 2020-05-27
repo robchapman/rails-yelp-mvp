a3fcff101326b3a1542ce9a26e14062a52b3e5c9
Robert Chapman <robchapman94@gmail.com>
2020-05-27 14:42:26 +1000

    Added Models, Controllers and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+Faker::Config.locale = 'en-AU'
+10.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  phone_number = Faker::PhoneNumber.phone_number_with_country_code
+  category = Restaurant::CATEGORIES.sample
+  restaurant = Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+  rand(1..3).times do
+    content = Faker::Restaurant.review
+    rating = rand(0..5)
+    review = Review.new(content: content, rating: rating)
+    review.restaurant = restaurant
+    review.save
+  end
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
